065bb5f083d8f3fa96d44cec280240d67e6e451f
Aleksandar Hardi <sasahardi@gmail.com>
2020-03-19 22:57:41 +0100

    Register and Logout

--- app/src/main/java/com/myappcompany/hardi/thecheatingroom/MainActivity.java
+++ app/src/main/java/com/myappcompany/hardi/thecheatingroom/MainActivity.java
@@ -1,14 +1,67 @@
 package com.myappcompany.hardi.thecheatingroom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(R.string.app_name);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Check if user is signed in
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            sentToStartActivity();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.main_logout_btn:
+                mAuth.signOut();
+                sentToStartActivity();
+                break;
+        }
+        return true;
+    }
+
+    private void sentToStartActivity() {
+        startActivity(new Intent(MainActivity.this, StartActivity.class));
+        finish();
     }
 }
---
+++ app/src/main/java/com/myappcompany/hardi/thecheatingroom/RegisterActivity.java
@@ -0,0 +1,62 @@
+package com.myappcompany.hardi.thecheatingroom;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplayName, mEmail, mPassword;
+    private Button mCreateBtn;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mDisplayName = findViewById(R.id.input_name_text);
+        mEmail = findViewById(R.id.input_email_text);
+        mPassword = findViewById(R.id.input_password_text);
+        mCreateBtn = findViewById(R.id.reg_create_btn);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mDisplayName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+                
+                registerUser(name, email, password);
+            }
+        });
+    }
+
+    private void registerUser(String name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Something went wrong...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
